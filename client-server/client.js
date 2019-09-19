022dcb9902cbd052757f74256e3176af358eebff
Rodrigo Pereira <rodrigorpogo@gmail.com>
2019-09-19 00:17:49 -0300

    Tcp example

--- client-server/client.js
+++ client-server/client.js
@@ -9,7 +9,7 @@ client.connect(3000, 'localhost', (socket) => {
 client.on('data', function (data) {
     console.log('Received: ' + data);
     client.write('Hello, server! Good bye!')
-    // client.destroy(); // kill client after server's response
+    client.destroy(); // kill client after server's response
 })
 
 client.on('close', function () {
