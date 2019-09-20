0d931da6ac7e6b45caaa8ac3fa487933a8a8d549
Rodrigo Pereira <rodrigorpogo@gmail.com>
2019-09-20 02:41:28 -0300

    Singleton test worked

--- database_control.js
+++ database_control.js
@@ -4,42 +4,61 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+const DATABASE_BASE_PATH = __dirname + '/database'
+
 class DatabaseControl {
 
     constructor() {
-        this._bets = null;       
-        this._past_auctions = null;       
-        this._session = null;       
-        this._users = null;       
+        this._bets = null;
+        this._past_auctions = null;
+        this._session = null;
+        this._users = null;
     }
 
     async getBets() {
-        
-        if(this._bets) {
-            return this.bets;
-        } 
 
-        let read = await readFile(__dirname + '/database/bets.json')
-        return JSON.parse(read.toString());        
+        if (this._bets) {
+            console.log('get default')
+            return this._bets;
+        }
+
+        let read = await readFile(this._resolveDatabasePath('cache/bets'))
+        this._bets = JSON.parse(read.toString());
+        return this._bets;
     }
 
     async setBets(bets) {
-        
-        if(!!bets) {
-            await writeFile(__dirname + '/datebase/bets.json', JSON.stringify(bets));            
-        }
-    }       
+        this._bets = bets;
 
-  
+        if (!!bets) {
+            try {
+                await writeFile(this._resolveDatabasePath('cache/bets'), JSON.stringify(bets));
+            }
+            catch (e) {
+                console.log(e)
+                this._retryOperation(bets, this.setBets);
+            }
+        }
+    }
+
+    _retryOperation(object, callback) {
+        setTimeout(function () {
+            callback(object);
+        }, 2000);
+    }
+
+    _resolveDatabasePath(db) {
+        return `${DATABASE_BASE_PATH}/${db}.json`
+    }
 }
 
 class Singleton {
 
-    constructor() {        
-        if(!Singleton._instance) {
+    constructor() {
+        if (!Singleton._instance) {
             Singleton._instance = new DatabaseControl();
-        } 
-    } 
+        }
+    }
 
     getInstance() {
         return Singleton._instance;
--- index.js
+++ index.js
@@ -1,12 +1,61 @@
-const fs = require('fs');
-const readFile = require('util').promisify(fs.readFile);
-const writeFile = require('util').promisify(fs.writeFile);
 const net = require('net')
 const DatabaseControl = require('./database_control')
 
 const databaseControl = new DatabaseControl().getInstance();
+const databaseControl2 = new DatabaseControl().getInstance();
 
-async function bootstrap() {
-    console.log(await databaseControl.getBets())
+
+async function bootstrap(name) {
+    let data = await databaseControl.getBets();
+    console.log(data)
+    await databaseControl.setBets({
+        ['23ipo312pio']: data['23ipo312pio']
+    });
+    const data2 = await databaseControl2.getBets();
+    data = await databaseControl.getBets();
+    console.log(data)
+    console.log(data2)
 }
 
+bootstrap('thiago');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ======================== TERMINAL SERVIDOR ====================
+// // Esperando conexões
+
+
+
+
+
+
+
+
+// // ======================== TERMINAL CLIENTE ====================
+// // Conectar no servidor
+// // Criar um leilão ou dar um lance
+// // Dar um lance
+
+// // Lista dos leilões
+// 1 - Carro - 20.0
+// 2 - Moto - 15.0
+// 3 - Fogão - 200.0
+
+// // Digite um leilao
+// 1
+
+// // Lista dos leilões
+// 1 - Carro - 20.0
+
+// // Digite o valor
+// 20
