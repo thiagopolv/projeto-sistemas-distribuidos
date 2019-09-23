6bba553ec41b96be3c973107f6eedb761c9f681b
thiagooliveirazup <thiago.oliveira@zup.com.br>
2019-09-22 21:00:45 -0300

    refactoring and started implementing server with broadcast

--- client-server/client.js
+++ client-server/client.js
@@ -1,17 +1,33 @@
 const net = require('net');
 
-const client = new net.Socket();
+const nickname = 'participant1';
 
-client.connect(3000, 'localhost', (socket) => {
-    console.log('Connected')
-})
 
-client.on('data', function (data) {
-    console.log('Received: ' + data);
-    client.write('Hello, server! Good bye!')
-    client.destroy(); // kill client after server's response
-})
 
-client.on('close', function () {
-    console.log('Connection closed');
-});
+function createSocket() {
+    const client = new net.Socket();
+    return client;
+}
+
+function startConnection(client, serverPort, serverIP) {
+    client.connect(serverPort, serverIP, (socket) => {
+        console.log('Connected')
+    })
+}
+
+function testExample() {
+    let client = createSocket();
+    startConnection(client, 3000, 'localhost');
+
+    client.on('data', function (data) {
+        console.log('Received: ' + data);
+        client.write('Hello, server! Good bye!')
+        //client.destroy(); // kill client after server's response
+    })
+
+    client.on('close', function () {
+        console.log('Connection closed');
+    });
+}
+
+testExample();
--- client-server/server.js
+++ client-server/server.js
@@ -2,27 +2,80 @@ const net = require('net');
 const createServer = require('util').promisify(net.createServer);
 
 let clientCount = 0;
+let clients = [];
 
-const server = net.createServer((socket) => {
-    socket.setDefaultEncoding('utf-8')
-    socket.on('data', function (data) {
-        console.log(data.toString())
-    })
-});
+const randomPort = 3000;
+randomIP = 'localhost';
+const startingBid = 10.00;
+const productName = 'Carro';
+const minimumNumberOfParticipants = 3;
+const auctionOwner = 'RandonOwner';
 
-server.on('connection', (socket) => {
-    clientCount++;
-    socket.write('Hello Client! Love to be server!');
-    if (clientCount > 2) {
-        server.close();
+function startAuction() {
+    const server = createAuctionServer();
+
+    server.listen(randomPort, randomIP, () => {
+        console.log(`Iniciando o leilão do ${productName} de ${auctionOwner}.`);
+        console.log(`São necessários ${minimumNumberOfParticipants} participantes para começar.`);
+        console.log(`O lance inicial é de R$${startingBid}`);
+    });
+
+    onConnectionEvent(server);
+}
+
+function createAuctionServer() {
+    return net.createServer((socket) => {
+        socket.setDefaultEncoding('utf-8')        
+    });
+}
+
+function onConnectionEvent(server) {
+    server.on('connection', (socket) => {
+        clients.push(socket);
+        console.log('Um novo participante se conectou ao leilão.');
+        broadcast('Um novo participante se conectou ao leilão.');
+        clientCount++;
+        validateNumberOfParticipants();
+    });
+}
+
+function broadcast(message) {
+    clients.forEach(function(client) {
+        client.write(message);
+    })      
+}
+
+function validateNumberOfParticipants() {
+    if(clientCount == minimumNumberOfParticipants) {
+        console.log('Número mínimo de participantes atingido. Iniciando leilão...');
     }
-});
+}
 
-server.on('close', () => {
-    console.log('Good bye');
-})
+function testExample() {
+    const server = net.createServer((socket) => {
+        socket.setDefaultEncoding('utf-8')
+        socket.on('data', function (data) {
+            console.log(data.toString())
+        })
+    });
 
-// Grab an arbitrary unused port.
-server.listen('3000', 'localhost', () => {
-    console.log('opened server on', server.address());
-});
+    server.on('connection', (socket) => {
+        clientCount++;
+        socket.write('Hello Client! Love to be server!');
+        if (clientCount > 2) {
+            server.close();
+        }
+    });
+
+    server.on('close', () => {
+        console.log('Good bye');
+    })
+
+    // Grab an arbitrary unused port.
+    server.listen('3000', 'localhost', () => {
+        console.log('opened server on', server.address());
+    });
+}
+
+startAuction();
+//testExample();
