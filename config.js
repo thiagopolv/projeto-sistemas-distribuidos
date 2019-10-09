462d680d18418f51f616389848d5f8f69f1ad949
Rodrigo Pereira <rodrigorpogo@gmail.com>
2019-10-09 01:41:31 -0300

    Generalizing configs

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    negativeAnswers: ['no', 'n', 'nao', 'não'],
+    clientPort: 3000,
+    ipToConnect: 'localhost',
+    serverPort: 3000,
+    serverIpAnswering: '',
+    minimumNumberOfParticipants: 1
+}
