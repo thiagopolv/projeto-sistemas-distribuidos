084a288bd3ccced922da88816b5cde9f2fc99d73
thiagopolv <thiagop.olv@gmail.com>
2019-10-23 00:17:56 -0300

    refactor

--- entrega2-grpc/src/main/java/server/AuctionServer.java
+++ entrega2-grpc/src/main/java/server/AuctionServer.java
@@ -2,15 +2,20 @@ package server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import util.ConfigProperties;
 
 import java.io.IOException;
+
+import static util.ConfigProperties.getServerPort;
 
 public class AuctionServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        ConfigProperties configProperties = ConfigProperties.getProperties();
+
         Server server = ServerBuilder
-                .forPort(8080)
+                .forPort(getServerPort())
                 .addService(new AuctionServiceImpl()).build();
 
         server.start();
